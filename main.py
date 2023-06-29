dbdc01647673edf7bfc8b2fb545cf26539364b1e
Mateo Echavarria Sierra <63327224+TeoEchavarria@users.noreply.github.com>
2023-06-29 10:09:49 -0500

    Update Projecto : Mostrando Jason
    - Añado archivo : Products
    - Products : Simulacion de base de datos
    - Ruta Ping

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#importar productos
+from products import products
+
 @app.route('/')
 def hello():
     return 'Hello World Flask'
 
+@app.route('/ping')
+def ping():
+    return products
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
---
+++ products.py
@@ -0,0 +1,5 @@
+products = [
+    {'name': 'laptop', 'price':20000},
+    {'name': 'phone', 'price':10000},
+    {'name': 'GamerPC', 'price':40000},
+]
